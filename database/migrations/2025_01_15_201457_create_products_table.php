0646ccb07da4e1db7972a2c174e58e2785aa7794
Luke Kalyan Brata Sarker <dhrubo.luke@gmail.com>
2025-01-16 02:32:28 +0600

    Model and Migration files of the database tables have been created

--- database/migrations/2025_01_15_201457_create_products_table.php
+++ database/migrations/2025_01_15_201457_create_products_table.php
@@ -11,14 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('product_id')->unique();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->integer('stock')->nullable();
+            $table->string('image');
+            $table->timestamps(); 
         });
     }
 
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
